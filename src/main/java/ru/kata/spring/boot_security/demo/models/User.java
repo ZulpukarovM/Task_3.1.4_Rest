7e3b53e2064d3cad43b34aa13902bc2800ba4650
Magomed Zulpukarov <zulpukarovmagomed00@gmail.com>
2024-10-10 19:10:27 +0300

    Немного переделал

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -18,7 +18,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
